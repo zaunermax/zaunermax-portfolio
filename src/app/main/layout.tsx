a891f73b003cc100c5f42f579205964e2976e6fa
Maximilian Zauner <zaunermax@gmail.com>
2023-08-02 12:31:19 +0200

    fix(layout): move footer to main layout

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -1,6 +1,12 @@
 import { ReactNode } from 'react';
 import { AppLayout } from '@/components/app-layout';
+import { Footer } from './components/footer';
 
 export default function MainLayout({ children }: { children: ReactNode }) {
-	return <AppLayout>{children}</AppLayout>;
+	return (
+		<>
+			<AppLayout>{children}</AppLayout>
+			<Footer />
+		</>
+	);
 }
