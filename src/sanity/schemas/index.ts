5684b8c6ef19447cce381e40081b54833e4ba47b
Maximilian Zauner <zaunermax@gmail.com>
2023-08-08 17:14:33 +0200

    feat(sanity): remove person schema

--- src/sanity/schemas/index.ts
+++ src/sanity/schemas/index.ts
@@ -1,6 +1,5 @@
-import person from './person';
 import llmContent from './llm-content';
 import wikiPage from './wiki-page';
 import general from './general';
 
-export const schemaTypes = [person, llmContent, wikiPage, general];
+export const schemaTypes = [llmContent, wikiPage, general];
