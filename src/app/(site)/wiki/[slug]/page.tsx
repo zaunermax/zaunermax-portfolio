109d81440f558e1b67a68bdb7ec2cf0b227db54f
Maximilian Zauner <zaunermax@gmail.com>
2023-08-09 09:31:08 +0200

    fix(vercel): added debug statement

--- src/app/(site)/wiki/[slug]/page.tsx
+++ src/app/(site)/wiki/[slug]/page.tsx
@@ -6,10 +6,12 @@ import { serverURL } from '@/lib/server-url';
 import { redirect } from 'next/navigation';
 import { getFetchCatchHandler, getFetchErrorHandler } from '@/lib/fetch-utils';
 
-const fetchWikiPageContent = (filename: string) =>
-	fetch(`${serverURL}/api/file/${filename}`, { next: { revalidate: 300 } })
+const fetchWikiPageContent = (filename: string) => {
+	console.log('DEBUG:', filename, serverURL);
+	return fetch(`${serverURL}/api/file/${filename}`, { next: { revalidate: 300 } })
 		.then(getFetchErrorHandler<WikiPageContent>())
 		.catch(getFetchCatchHandler(null));
+};
 
 const Page = async ({ params }: { params: { slug: string } }) => {
 	const res = await fetchWikiPageContent(params.slug);
