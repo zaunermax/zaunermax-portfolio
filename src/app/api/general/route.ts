47cc2f21cad83d69842b065c607cd9a071cadc76
Maximilian Zauner <zaunermax@gmail.com>
2023-08-09 20:17:42 +0200

    fix(cache): fixed cache issues
    - apparently removing edge did do the trick

--- src/app/api/general/route.ts
+++ src/app/api/general/route.ts
@@ -1,10 +1,10 @@
 import { getGeneralInfo } from '@/lib/get-general-info';
 import { NextResponse } from 'next/server';
+import { globalRevalidate } from '@/lib/global-revalidate';
 
 export async function GET() {
 	const res = await getGeneralInfo();
 	return NextResponse.json(res);
 }
 
-export const revalidate = 300;
-export const runtime = 'edge';
+export const revalidate = globalRevalidate;
