4e5fd67fa38fb3db621bf033ca7e8e8add95442b
Maximilian Zauner <zaunermax@gmail.com>
2024-01-19 00:34:21 +0100

    feat(sitemap): implemented sitemap properly

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,9 @@
 import { MetadataRoute } from 'next';
 import { serverURL } from '@/lib/server-only/server-url';
+import { getWikiContent } from '@/lib/get-wiki-content';
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+	const content = await getWikiContent();
 	return [
 		{
 			url: serverURL,
@@ -15,5 +17,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
 			url: `${serverURL}/wiki`,
 			lastModified: new Date(),
 		},
+		{
+			url: `${serverURL}/impressum`,
+			lastModified: new Date(),
+		},
+		...content.map((c) => ({
+			url: `${serverURL}/wiki/${c.filename}`,
+			lastModified: new Date(),
+		})),
 	];
 }
