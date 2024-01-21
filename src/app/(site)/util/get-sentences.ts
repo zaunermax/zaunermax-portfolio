873d585e1258be4417e1ef4841f464bf29710075
Maximilian Zauner <zaunermax@gmail.com>
2024-01-21 11:22:33 +0100

    feat(sentences): add randomness to sentences
    - sentences can now be random if specified
    - the cms now provides a isRandom flag to sentences

--- src/app/(site)/util/get-sentences.ts
+++ src/app/(site)/util/get-sentences.ts
@@ -1,7 +1,8 @@
 import { getGeneralInfo } from '@/lib/get-general-info';
 import { prepareTypeAnimationArray } from '@/lib/prepare-type-animation-array';
+import { getIntroSentencesV2 } from '@/lib/get-intro-sentences-v2';
 
 export const getSentences = () =>
-	getGeneralInfo().then(({ introSentences }) =>
-		prepareTypeAnimationArray(introSentences, 3000, 5000),
+	getIntroSentencesV2().then(({ introSentencesV2 }) =>
+		prepareTypeAnimationArray(introSentencesV2, 3000, 5000),
 	);
