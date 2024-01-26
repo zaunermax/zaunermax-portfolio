89d682b18da7c5b720322c659af8e0bdcf508a45
Maximilian Zauner <zaunermax@gmail.com>
2024-01-26 16:06:57 +0100

    fix(wiki): adapted file row spacings

--- src/components/file-display/file-row.tsx
+++ src/components/file-display/file-row.tsx
@@ -15,12 +15,12 @@ export const FileRow = ({
 	icon: Icon,
 }: FileRowProps) => {
 	return (
-		<div className="flex items-center border-t-[1px] border-opacity-15 border-black px-3 py-3 text-xs hover:cursor-pointer hover:bg-wiki-header-light dark:border-t-white dark:border-opacity-10 hover:dark:bg-wiki-header">
+		<div className="flex items-center border-t-[1px] border-black border-opacity-15 px-3 py-3 text-xs hover:cursor-pointer hover:bg-wiki-header-light dark:border-t-white dark:border-opacity-10 hover:dark:bg-wiki-header">
 			<Icon className="mr-4 h-4 w-4" />
-			<div className="w-100 flex-auto basis-1/6 hover:cursor-pointer hover:text-blue-500 hover:underline">
+			<div className="w-100 flex-auto basis-1/5 hover:cursor-pointer hover:text-blue-500 hover:underline">
 				{filename}
 			</div>
-			<div className="w-100 hidden flex-auto basis-1/3 text-gray-500 hover:cursor-pointer hover:text-blue-500 hover:underline sm:block">
+			<div className="w-100 hidden flex-auto basis-1/6 text-gray-500 hover:cursor-pointer hover:text-blue-500 hover:underline sm:block">
 				{commitMsg}
 			</div>
 			<div className="basis-auto flex-nowrap text-right text-gray-500 sm:basis-1/6">
