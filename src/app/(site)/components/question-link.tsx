421013296bedd4f2f29f224bd189b5be6eab334f
Maximilian Zauner <zaunermax@gmail.com>
2025-05-13 20:05:26 +0200

    feat(ui): enhance loading fallback and adjust page layout

--- src/app/(site)/components/question-link.tsx
+++ src/app/(site)/components/question-link.tsx
@@ -1,10 +1,22 @@
 import { Suggestions } from './suggestions';
 import { QuestionFormLink } from './question-form-link';
 import { Suspense } from 'react';
+import { Badge } from '@/components/ui/badge';
 
-const SuggestionFallback = () => {
-	return <div>Loading some suggestions...</div>;
-};
+const SuggestionFallback = () => (
+	<div>
+		{Array.from({ length: 3 }).map((_, idx) => (
+			<span key={idx} className="mr-2">
+				<Badge
+					className="mt-1 w-full rounded-2xl px-4 py-2 md:m-auto md:mb-2 md:w-auto md:rounded-full md:px-2.5 md:py-0.5"
+					variant="secondary"
+				>
+					<span className="inline-block w-60 rounded">&nbsp;</span>
+				</Badge>
+			</span>
+		))}
+	</div>
+);
 
 export async function QuestionLink() {
 	return (
